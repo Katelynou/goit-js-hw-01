c11537203f7c6ea5f9ca0ee1117e7643ef777363
katelynou <drobatiy@gmail.com>
2024-03-23 19:32:00 +0200

    fin

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let quantity;
 function makeTransaction(quantity, pricePerDroid) {
     let totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`
